7cf0f95e2cba7533ce77c25bed57d3dd205dfa25
Shahtr1 <tramboos123@gmail.com>
2026-09-21 20:16:34 +0530

    started working on backend

---
+++ backend/src/test/java/com/shahbytes/chathub/ChathubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahbytes.chathub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChathubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
